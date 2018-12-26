086eef73dfbe6a81e85bcea2da22dfa237cdc8c5
Stig Nygaard <stig@rockland.dk>
2018-12-26 20:14:54 +0100

    var declarations

--- ShowOnGoogleMaps.jsx
+++ ShowOnGoogleMaps.jsx
@@ -5,22 +5,22 @@ if( BridgeTalk.appName === "bridge" ) {
 mapFromGPS.onSelect = function () {
     if(app.document.selections.length <1) return;
     var thumb = app.document.selections[0];
-    md = thumb.synchronousMetadata;
+    var md = thumb.synchronousMetadata;
     md.namespace =  "http://ns.adobe.com/exif/1.0/";
-    lat = md.GPSLatitude ? md.GPSLatitude : "" ;
-    lon = md.GPSLongitude ? md.GPSLongitude : "";
+    var lat = md.GPSLatitude ? md.GPSLatitude : "" ;
+    var lon = md.GPSLongitude ? md.GPSLongitude : "";
     if(lat === "") return;
     if(lon === "") return;
     var latparts = lat.match(/\d+/g);
-    lat1 =( Number(latparts[0]) +(Number(latparts[1].toString()+"."+latparts[2].toString())/60)).toFixed(4);
+    var lat1 =( Number(latparts[0]) +(Number(latparts[1].toString()+"."+latparts[2].toString())/60)).toFixed(4);
     var lonparts = lon.match(/\d+/g);
-    lon1 = (Number(lonparts[0]) +(Number(lonparts[1].toString()+"."+lonparts[2].toString())/60)).toFixed(4);
-    latneg='';
+    var lon1 = (Number(lonparts[0]) +(Number(lonparts[1].toString()+"."+lonparts[2].toString())/60)).toFixed(4);
+    var latneg='';
     switch(lat.match(/.$/).toString()){
         case  'S' : latneg='-'; break;
         case  'W' : latneg='-'; break;
     }
-    lonneg='';
+    var lonneg='';
     switch(lon.match(/.$/).toString()){
         case  'S' : lonneg= '-'; break;
         case  'W' : lonneg= '-'; break;
